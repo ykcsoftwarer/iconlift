783c717ba7d6f0514a6625e633bad9f395f7a490
duygukucukoglu35 <duygukucukoglu35@gmail.com>
2023-11-09 15:45:10 +0300

    hakkimizda detail done

--- src/pages/Hakkimizda.js
+++ src/pages/Hakkimizda.js
@@ -2,7 +2,7 @@ import React from 'react';
 import HeaderThree from '../common/header/HeaderThree';
 import Breadcrumb from '../common/breadcrumb/Breadcrumb';
 import WhyChooseFour from '../components/why-choose-us/WhyChooseFour';
-import FeatureTwo from '../components/features/FeatureTwo';
+import HakkimizdaDetail from '../components/features/HakkimizdaDetail';
 import ProjectOne from '../components/project/ProjectOne';
 import ServiceFour from '../components/services/ServiceFour';
 import BrandOne from '../components/brand/BrandOne';
@@ -13,15 +13,11 @@ const ServicesPage = () => {
     return (
         <>
             <HeaderThree />
-            <Breadcrumb
-                heading="Hakkımızda"
-                currentPage="İconlift Asansör, Konya'da kurulmuş, asansör sektöründe kaliteli ve güvenli ürünler tedarik eden ve ihraç eden bir firmadır. Müşterilerimizin can ve mal güvenliğini sağlamak, yaşam kalitesini arttırmak ve ülke ekonomisine katkıda bulunmak vizyonumuzun temelini oluşturmaktadır. Koşulsuz müşteri memnuniyeti ilkesiyle çalışan tecrübeli ve profesyonel ekibimiz, yurtiçi ve yurtdışında asansör hizmeti vermektedir. " 
-            />
+            <HakkimizdaDetail />
             <WhyChooseFour />
-            <FeatureTwo />
-            <ProjectOne />
-            <ServiceFour />
-            <BrandOne />
+            {/* <ProjectOne /> */}
+            {/* <ServiceFour /> */}
+            {/* <BrandOne /> */}
             <FooterOne />
         </>
     )
