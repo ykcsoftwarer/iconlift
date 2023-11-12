badbc18f6ce1c680dad521a7099afd3ea319e4eb
ykcsoftwarer <yusufkucukcopur@gmail.com>
2023-11-12 22:14:06 +0300

    slayder

--- src/components/hero/HeroOne.js
+++ src/components/hero/HeroOne.js
@@ -47,8 +47,8 @@ export default class HeroOne extends React.Component {
                                 <div className="row clearfix">
                                     <div className="col-xl-12">
                                         <div className="slider-one__single-content">
-                                            <h3>ICONLİFT</h3>
-                                            <h2>Sizler için üretiyoruz<br /></h2>
+                                            <h3>İCONLİFT</h3>
+                                            <h2>Sizler İçin Kaliteyi Yakalıyoruz<br /></h2>
                                             <p>
                                                 Motivasyonumuzu müşterilerimizin memnuniyetinden alıyoruz...
                     
