0e0c208e1fb493a017832ed047c53438a0b93f5f
Fatih Sarıkaya <f.sarikaya@gmail.com>
2023-12-07 13:49:59 +0300

    api

--- src/pages/HomeDefault.js
+++ src/pages/HomeDefault.js
@@ -23,7 +23,7 @@ const HomeDefault = () => {
             <AboutFour />
             <ProjectThree />
             <FeatureFour />
-            <FeatureThree />
+            {/* <FeatureThree /> */}
         
             <FooterOne />
         </>
