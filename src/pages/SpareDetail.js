40dd5a77a84f8cb3dd5ad715144f94d774e8af97
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-12-13 09:36:12 +0300

    spare added hum

---
+++ src/pages/SpareDetail.js
@@ -0,0 +1,81 @@
+import React, { useEffect } from 'react';
+import {Link, useParams} from 'react-router-dom';
+import { useIconContext } from '../context/Context';
+import HeaderThree from '../common/header/HeaderThree';
+import FooterOne from '../common/footer/FooterOne';
+
+const SpareDetail = () => {
+ 
+ 
+    const {slug}= useParams()
+    const {fetchPageData, page ,lang}= useIconContext()
+    useEffect(() => {
+        fetchPageData(slug)
+    },[lang])
+    const modulPage = page?.filter((page) => page?.modulName === "productdetails")
+    const data = modulPage[0]?.data;
+        let publicUrl = process.env.PUBLIC_URL+'/'
+        return (
+            <>
+             <HeaderThree />
+           
+                {/* <section className="services-three">
+                    <div className="shape1"><img src={publicUrl+"assets/images/shapes/services-v3-shape1.png"} alt="#" /></div>
+                    <div className="shape2 rotate-me"><img src={publicUrl+"assets/images/update1.0/services-v3-shape2.png"} alt="#" /></div>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-xl-12">
+                                         
+                                <div className="services-three__top">
+                                    <div className="sec-title">
+                                        <div className="sec-title__tagline">
+                                        <span className="left"></span> <h6>İCONLİFT</h6> <span className="right"></span>
+                                        </div>
+                                        <h2 className="sec-title__title"> <br /> Asansör Sistemleri</h2>
+                                    </div>
+                             
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="row">
+{categoriesData?.map((k,index)=>{
+return (
+    <div className="col-xl-3 col-lg-6 col-md-6">
+    <div className="services-three__single">
+        <div className="services-three__single__bg"
+         style={{backgroundImage: `url(${k.image})`}}
+        ></div>
+        <div className="services-three__single-icon">
+            <span className="icon-crane"></span>
+        </div>
+        <h3 className=''>
+            <Link 
+           to={`/${asansorPath}/${k?.slug}`}>{k.title}</Link></h3>
+        <p>Lorem ipsum is simply free text dolor amett consec adipisc.</p>
+        <div className="btn-box">
+            <Link 
+            to={`/${asansorPath}/${k?.slug}`}>
+                <span className="icon-right-arrow"></span></Link>
+        </div>
+    </div>
+</div>
+)
+
+})}
+                           
+                         
+
+                          
+                        </div>
+                    </div>
+                </section> */}
+
+              <img src={data?.image} alt="" />
+                <FooterOne />
+            </>
+        )
+  
+}
+
+export default SpareDetail
