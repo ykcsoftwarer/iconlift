1444262bc832b30f9b9945acce2a5637670c5296
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-12-14 16:26:12 +0300

    elevator content edite

--- src/pages/AsansörDetail.js
+++ src/pages/AsansörDetail.js
@@ -3,20 +3,22 @@ import HeaderThree from "../common/header/HeaderThree";
 import Breadcrumb from "../common/breadcrumb/Breadcrumb";
 
 import FooterOne from '../common/footer/FooterOne';
-import BrandOne from '../components/brand/BrandOne';
+
 import SingleAsansörContracting from '../Kişiler/Hümeyra/asansor/SingleAsansörContracting';
 import { useParams } from 'react-router-dom';
+import BreadcrumbElevator from '../common/breadcrumb/BreadcrumbElevator';
 
 const AsansörDetail = () => {
-  const { categorySlug,slug} = useParams();
+  const { slug} = useParams();
+  console.log("hh",slug)
   return (
     <>
     <HeaderThree />
-    <Breadcrumb
-        heading={slug} currentPage={categorySlug}
+    <BreadcrumbElevator
+        heading={slug} 
     />
     <SingleAsansörContracting />
-    <BrandOne />
+   
     <FooterOne />
 </>
   )
--- src/pages/SparePart.js
+++ src/pages/SparePart.js
@@ -3,14 +3,18 @@ import HeaderThree from '../common/header/HeaderThree'
 import Breadcrumb from '../common/breadcrumb/Breadcrumb'
 import FooterOne from '../common/footer/FooterOne'
 import Spare from '../Kişiler/Hümeyra/yedek/Spare'
+import { useParams } from 'react-router-dom'
+import BreadcrumbSpare from '../common/breadcrumb/BreadcrumbSpare'
 
 const SparePart = () => {
+  const { categorySlug,slug} = useParams();
+  console.log("yy",categorySlug)
+
   return (
     <>
         <HeaderThree />
-        <Breadcrumb
-                heading="Yedek Parcalar"
-                currentPage= "Yedek Parçalar"  
+        <BreadcrumbSpare
+                heading="Yedek Parça Çeşitleri"  
             />
             <Spare/>
               <FooterOne />
